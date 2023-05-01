98f2badb9a0cd7debfe09712834b2793c5ed284e
IrvingCM123 <98989957+IrvingCM123@users.noreply.github.com>
2023-04-30 18:38:31 -0600

    cambios

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,14 @@
 export const environment = {
   production: true,
   apiUrl:'https://my-json-server.typicode.com/IrvingCM123/Consumir_Apis',
-  apiMensaje: 'http://localhost:2000/Servidor/enviar/'
+  apiMensaje: 'http://localhost:2000/Servidor/enviar/',
+  firebase: {
+    apiKey: 'AIzaSyDIahzHtIBUdWUOWGam_MesAxCUBgkcQkg',
+    authDomain: "sistemaasistencia-9d5e1.firebaseapp.com",
+    projectId: "sistemaasistencia-9d5e1",
+    storageBucket: "sistemaasistencia-9d5e1.appspot.com",
+    messagingSenderId: "245812703918",
+    appId: "1:245812703918:web:68c6faf48e5971ab5c333f",
+    measurementId: "G-V4BQXPDH5X"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   apiUrl:'https://my-json-server.typicode.com/IrvingCM123/Consumir_Apis',
   apiMensaje: 'http://localhost:2000/Servidor/enviar/',
   firebase: {
