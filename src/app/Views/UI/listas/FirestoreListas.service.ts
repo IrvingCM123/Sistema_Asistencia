06d6dbe11fae71a0db42852171fbd1cd4ef7d76a
IrvingCM123 <98989957+IrvingCM123@users.noreply.github.com>
2023-05-23 22:36:37 -0600

    cambios

--- src/app/Views/UI/listas/FirestoreListas.service.ts
+++ src/app/Views/UI/listas/FirestoreListas.service.ts
@@ -32,12 +32,11 @@ export class FirestoreService {
   }
 
   async getListaAsistencia(nrc: string, carrera: string) {
-
     let url = '/' + carrera + '/Materias/' + nrc;
     const lista_encontrada = await this.firestore.collection(url).get().toPromise();
     console.log(lista_encontrada)
     if (lista_encontrada) {
-      const datos_lista = lista_encontrada.docs.map((alumnos) => alumnos.data());
+      const datos_lista = await lista_encontrada.docs.map((alumnos) => alumnos.data());
       console.log(datos_lista);
       return datos_lista;
     } else {
