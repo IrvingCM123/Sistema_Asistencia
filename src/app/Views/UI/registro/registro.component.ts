f883e8a2ef2ca94c0651ef7fe2dfb31995638f5a
IrvingCM123 <98989957+IrvingCM123@users.noreply.github.com>
2023-05-24 08:29:11 -0600

    Registro

--- src/app/Views/UI/registro/registro.component.ts
+++ src/app/Views/UI/registro/registro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FirestoreService } from '../listas/FirestoreListas.service';
 
 @Component({
   selector: 'app-registro',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
-  constructor() { }
+  constructor(private datosLocales: FirestoreService) { }
+
+  username: string = "";
+  password: string = "";
+  loginFailed: boolean = false;
+  loggedIn: boolean = false;
+
+  login(): void {
+    // Aquí puedes agregar la lógica para validar las credenciales ingresadas.
+    // Por simplicidad, este ejemplo compara el username con "admin" y la password con "password".
+
+    if (this.username === "admin" && this.password === "password") {
+      this.datosLocales.guardar_DatoLocal("loggedIn", true);
+      this.loggedIn = this.datosLocales.obtener_DatoLocal("loggedIn");
+    } else {
+      this.datosLocales.guardar_DatoLocal("loggedIn", false);
+      this.loginFailed = true;
+      this.loggedIn = false;
+    }
+  }
 
   ngOnInit(): void {
+
   }
 
 }
