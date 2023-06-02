25340a0757dd91190e2c375c3a9b2314e7e659f5
IrvingCM123 <98989957+IrvingCM123@users.noreply.github.com>
2023-06-02 00:15:36 -0600

    cambios

--- src/app/Views/UI/listas/listas.component.ts
+++ src/app/Views/UI/listas/listas.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { DatosService } from '../inicio/Datos.Service';
-import { FirestoreService } from './FirestoreListas.service';
-import { DatosServicel } from './DatosServiceL.Service';
+import { HttpClient } from '@angular/common/http';
 import { GetListaAsistenciaUseCase } from 'src/app/domain/ListaAsistencia/usecase/getLista';
+import * as XLSX from 'xlsx';
+import { saveAs } from 'file-saver';
 
 interface Dato {
   id?: string;
@@ -11,6 +12,7 @@ interface Dato {
   Nombre: string;
   Status: string;
 }
+
 @Component({
   selector: 'app-listas',
   templateUrl: './listas.component.html',
@@ -20,11 +22,16 @@ export class Listas implements OnInit {
   listaAsistencia: any[] | any= [];
   nrcMateria: any;
   carrera: any;
+  jsonData: any;
 
+  fileToUpload: File | any;
+  filePreview: boolean | any;
+  uploadSuccess: boolean | any;
 
   constructor(
     private _getListaAsistenciaCasosUso: GetListaAsistenciaUseCase,
     private datos: DatosService,
+    private http: HttpClient
   ) {
     this.carrera = datos.getCarrera();
     this.nrcMateria = datos.getNrc();
@@ -32,5 +39,46 @@ export class Listas implements OnInit {
 
   async ngOnInit() {
     this.listaAsistencia = await this._getListaAsistenciaCasosUso.getListaAsistenciaByNrcCarrera(this.nrcMateria, this.carrera);
+  }
+
+  onFileChange(event: any) {
+    const file = event.target.files[0];
+    this.readFile(file);
+  }
+
+  readFile(file: File) {
+    const reader = new FileReader();
+    reader.onload = (e: any) => {
+      const data = new Uint8Array(e.target.result);
+      const workbook = XLSX.read(data, { type: 'array' });
+      const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+      this.jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+      this.filePreview = true;
+      console.log(this.jsonData);
+    };
+    reader.readAsArrayBuffer(file);
+  }
+
+
+
+  uploadFile(event: any) {
+    event.preventDefault();
+
+    if (this.fileToUpload) {
+      const formData = new FormData();
+      formData.append('file', this.fileToUpload);
+
+      // Realiza la petición para enviar el archivo al servidor
+      this.http.post<any>('URL_DE_TU_API', formData).subscribe(
+        response => {
+          // Actualiza la lista de asistencia con los datos recibidos del servidor
+          this.listaAsistencia = response;
+          this.uploadSuccess = true;
+        },
+        error => {
+          console.error('Error al cargar el archivo:', error);
+        }
+      );
+    }
   }
 }
