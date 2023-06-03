6f58878df3bec6de558e4ce7a32b53a03780a440
IrvingCM123 <98989957+IrvingCM123@users.noreply.github.com>
2023-06-02 23:37:44 -0600

    cambios

--- src/app/Views/UI/listas/listas.component.ts
+++ src/app/Views/UI/listas/listas.component.ts
@@ -37,20 +37,30 @@ export class Listas implements OnInit {
     const file = event.target.files[0];
     this.readFile(file);
   }
-
   readFile(file: File) {
-    const reader = new FileReader();
+    const reader: any = new FileReader();
     reader.onload = (e: any) => {
-      const data = new Uint8Array(e.target.result);
-      const workbook = XLSX.read(data, { type: 'array' });
-      const worksheet = workbook.Sheets[workbook.SheetNames[0]];
-      this.jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+      const data: any = new Uint8Array(e.target.result);
+      const workbook: any = XLSX.read(data, { type: 'array' });
+      const worksheet: any = workbook.Sheets[workbook.SheetNames[0]];
+
+      const rangeD: any = XLSX.utils.sheet_to_json(worksheet, { range: 'D11:D36', header: 1 });
+      const rangeH: any = XLSX.utils.sheet_to_json(worksheet, { range: 'H11:H36', header: 1 });
+      const rangeO: any = XLSX.utils.sheet_to_json(worksheet, { range: 'O11:O36', header: 1 });
+      const rangeP: any = XLSX.utils.sheet_to_json(worksheet, { range: 'P11:P36', header: 1 });
+
+      const combinedData: any[] = [];
+      for (let i = 0; i < rangeD.length; i++) {
+        const row: any[] = [rangeD[i][0], rangeH[i][0], rangeO[i][0], rangeP[i][0]];
+        combinedData.push(row);
+      }
+
+      this.jsonData = combinedData;
       this.filePreview = true;
       console.log(this.jsonData);
     };
     reader.readAsArrayBuffer(file);
   }
-
 
 
   uploadFile(event: any) {
